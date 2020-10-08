c8918cfcf750035d0e5d934820f8b64a6d7c1e9d
Gideon Cooper <Gideon.Cooper@hotmail.com>
2020-10-08 17:32:59 +1300

    parsing query strings work and getting the result in the console log

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import queryString from 'query-string'
+import io from 'socket.io-client'
 
-const Chat = () => {
+const Chat = ({ location }) => {
+  useEffect(() => {
+    const query = queryString.parse(location.search)
+
+    console.log(query)
+  })
   return <h1>Chat</h1>
 }
 export default Chat
