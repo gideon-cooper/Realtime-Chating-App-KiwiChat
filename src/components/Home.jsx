5bbcf843b7ef8575e901328b9eb702c33e01d4ac
Gideon Cooper <Gideon.Cooper@hotmail.com>
2020-10-08 17:21:11 +1300

    added sass and initial sass file

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-
+import { Link } from 'react-router-dom'
 const Home = () => {
   const [name, setName] = useState('')
   const [room, setRoom] = useState('')
