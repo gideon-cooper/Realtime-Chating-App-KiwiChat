2ea4061e6a9dd0c4ce763044fbae51573b5c2753
Gideon Cooper <Gideon.Cooper@hotmail.com>
2020-10-09 15:58:09 +1300

    functionality working and bug with the same name on the messages fixed

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 
 export default function Message({ message, name }) {
-  return (
+  let currentUser = false
+
+  if (message.user === name) {
+    currentUser = true
+  }
+  return currentUser ? (
     <div>
       {message.text}
       {name}
+    </div>
+  ) : (
+    <div>
+      {message.text}
+      {message.user}
     </div>
   )
 }
